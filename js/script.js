5d430f62ec73f1eb14c369f9ee287d99aae83c63
genildocs <genildocs@gmail.com>
2023-04-24 03:51:51 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const form = document.getElementsByTagName('form')[0];
+const error = document.querySelector('.error');
+const cvc = document.getElementById('input-cvc');
+
+/* form.addEventListener("submit", function(event){
+    if(!cvc.validity.valid){
+        error.innerHTML = "Campo obrigatorio!"
+        event.preventDefault();
+    }
+},false);
+console.log(cvc.value); */
