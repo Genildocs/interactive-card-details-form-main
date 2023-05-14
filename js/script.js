2d4301afe12228b56a9e09ac14e644641bdccd8d
genildocs <genildocs@gmail.com>
2023-05-14 15:55:08 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -4,27 +4,36 @@
    2º Validar os campos de data e cvc se foram inserido apenas numeros e a quantidade correta de numeros
    3º Deixar o foco nos campos que não foram preenchidos corretamente ou que esteja faltando dados  */
 
+/* info das img dos card */
+const cardFrontNumber = document.querySelector('.heading-2');
+
+
+/* inputs do form */
 const form = document.getElementsByTagName('form')[0];
 const error = document.querySelector('.error');
+const overlay = document.querySelector('.overlay');
+const hidden = document.querySelector('.hidden');
 const inputName = document.getElementById('input-name');
-const inputNumber = document.getElementById('input-number');
-const dateNumber1 = document.getElementById('input-date-1');
-const dateNumber2 = document.getElementById('input-date-2');
-const cvcNumber = document.getElementById('input-cvc');
+const inputNumber = document.getElementById('input-number').value;
+const dateNumber1 = document.getElementById('input-date-1').value;
+const dateNumber2 = document.getElementById('input-date-2').value;
+const cvcNumber = document.getElementById('input-cvc').value;
 const complete = document.querySelector('.complete');
 const btn = document.querySelector('.btn');
-console.log(complete);
 
-function validaCardNumber(){
-  const inputNumber = parseInt(inputNumber.value);
-  console.log(inputNumber);
-}
+console.log(isNaN(inputNumber));
 
+/* function validaNumber(){
+   if(isNaN(inputNumber)){
+      return alert('Não é um numero')
+      
+   }else{return false}
+} */
 
-form.addEventListener("submit", function(){
-  
-    complete.style.display = "block";
+form.addEventListener('submit', function(event){
+   event.preventDefault();
+
+   form.submit();
+
 })
-
-
 
