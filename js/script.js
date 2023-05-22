f8daa96b264e283ceefc18fb79acbe2d2ed6037a
genildocs <genildocs@gmail.com>
2023-05-22 03:21:16 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,76 +1,97 @@
-'use strict';
+/* INPUTS FORM */
+const form = document.getElementsByTagName("form")[0];
+const inputName = document.getElementById("input-name");
+const inputNumber = document.getElementById("input-number");
+const inputCvc = document.getElementById("input-cvc");
+const inputDates = ["input-date-1", "input-date-2"];
+/* INPUTS ERROS AND COMṔLETE*/
+const error = document.querySelector(".error");
+const complete = document.querySelector(".complete");
 
-/* 1º Validar se o campo number só contem numeros
-   2º Validar os campos de data e cvc se foram inserido apenas numeros e a quantidade correta de numeros
-   3º Deixar o foco nos campos que não foram preenchidos corretamente ou que esteja faltando dados  */
+/* Buttons */
+const btn = document.querySelector(".btn");
+const btnComplete = document.querySelector(".btn-complete");
 
 /* info das img dos card */
-const cardFrontNumber = document.querySelector('.heading-2');
+const cardFrontNumber = document.querySelector(".heading-2");
+const cardFrontName = document.querySelector(".card-front__name");
 
-
-/* inputs do form */
-const form = document.getElementsByTagName('form')[0];
-const error = document.querySelector('.error');
-const overlay = document.querySelector('.overlay');
-const hidden = document.querySelector('.hidden');
-const inputName = document.getElementById('input-name');
-const complete = document.querySelector('.complete');
-const btnComplete = document.querySelector('.btn-complete');
-const cvcNumber = document.getElementById('input-cvc').value;
-
-
-/* Valida o campo input-number */
-function validaNumber() {
-
-    const inputNumber = document.getElementById('input-number').value;
-
-    if (inputNumber.length !== 16 || inputNumber === '') {
-        error.textContent = "Wrong format, numbers only"
-        return false;
-    } else {
-        if (isNaN(inputNumber)) {
-            error.textContent = "Wrong format, numbers only"
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-
+/* Insere informações dos inputs nos card images 
+   Inserts input information into the card images.*/
+/* function cardImages(input, image) {
+  input.addEventListener("input", function () {
+    image.textContent = input.value;
+  });
 }
 
-/* Valida o campo input-date */
+cardImages(inputNumber, cardFrontNumber);
+cardImages(inputName, cardFrontName); */
+
+/* Nova função */
+
+/* function inserInfo(){
+    
+    inputNumber.addEventListener('input', function(){
+        const cardFrontNumber = inputNumber.value;
+        const newArray = new Array(cardFrontNumber);
+        console.log(newArray);
+    })
+}
+
+inserInfo(); */
+
+/* Verifica se os campos de input number e input cvc são validos
+   Check if the input fields for 'number' and 'CVC' are valid. */
+function validaInput(valor) {
+  if (isNaN(valor) || valor === "") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/* Verifica se o campo input-date é valido
+   Check if the input field 'input-date' is valid. */
 function validaDate() {
-    const inputsDate = ['input-date-1', 'input-date-2'];
-    const errorsDate = document.querySelector('.error-date');    
-    let valid = true;
-    for (let i = 0; i < inputsDate.length; i++) {
-        const datesNumber = document.getElementById(inputsDate[i]).value;        
-        if (isNaN(datesNumber) || datesNumber === '') {
-            errorsDate.textContent = "Wrong format, numbers only"            
-            valid = false;
-        } 
-    }
+  let valid = true;
+  for (let i = 0; i < inputDates.length; i++) {
+    const inputsValue = document.getElementById(inputDates[i]).value;
 
-    return valid;
+    if (isNaN(inputsValue) || inputsValue === "") {
+      valid = false;
+    }
+  }
+
+  return valid;
 }
 
+function validaName() {
+  const nameValue = inputName.value;
+  console.log(nameValue);
+}
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
+function showForm() {
+  form.classList.toggle("hidden");
+  complete.classList.toggle("hidden");
+}
 
+function showError() {
+  error.textContent = "Enter numbers only.";
+}
 
-    if (validaNumber() && validaDate()) {
-        form.classList.toggle('hidden');
-        complete.classList.toggle('hidden');
-    }
-})
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  validaName();
 
-btnComplete.addEventListener('click', function () {
-    form.classList.toggle('hidden');
-    complete.classList.toggle('hidden');
-    document.location.reload();
-})
+  if (
+    validaInput(inputNumber.value) &&
+    validaInput(inputCvc.value) &&
+    validaDate()
+  ) {
+    showForm();
+  } else {
+    showError();
+  }
+});
 
-
-
+btnComplete.addEventListener("click", showForm);
